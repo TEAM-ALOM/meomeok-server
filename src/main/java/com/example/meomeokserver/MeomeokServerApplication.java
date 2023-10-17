55697e87aaa5d5dbd207d3a48eebd90184b92e8b
vthyrachel <vthyrachel@naver.com>
2023-10-17 22:57:00 +0900

    Initial Commit

---
+++ src/main/java/com/example/meomeokserver/MeomeokServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.meomeokserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeomeokServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeomeokServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/meomeokserver/MeomeokServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meomeokserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeomeokServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
